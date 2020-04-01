e80b4599b58835639dad1dda03fe43f8e27b0ae8
justinro-underscore <jcroderman@gmail.com>
2020-04-01 16:51:39 -0500

    Adds comments to inventory and event handlers

--- src/handler/eventHandler.ts
+++ src/handler/eventHandler.ts
@@ -1,12 +1,23 @@
 import { GameObject } from "../gameobjects/gameObject";
 import { TestingEventMap } from "../gameinfo/events/testingEvents";
 
+/**
+ * Handles all events
+ * An event is described as a manipulation of the game world due to using two objects
+ */
 export class EventHandler {
-  private static eventMap: Map<string, () => string>;
+  private static eventMap: Map<string, () => string>; // Links use objects (format: "${ use_obj } ${ with_obj }") with their event
+  /*
+   * Can have multiple event maps for different scenarios. Links keys with their event maps
+   */
   private static availableEventMaps: Map<string, {events: {useObj: string, withObj: string, event: () => string}[]}> = new Map([
     ["testing", TestingEventMap]
   ]);
 
+  /**
+   * Sets the currently used event map based on a key
+   * @param eventMapKey The key relating to the event map to instantiate
+   */
   static instantiateEventMap(eventMapKey: string) {
     if (!EventHandler.availableEventMaps.has(eventMapKey)) {
       console.error(`Event Map {${ eventMapKey }} does not exist!`)
@@ -14,11 +25,17 @@ export class EventHandler {
     else {
       EventHandler.eventMap = new Map<string, () => string>();
       EventHandler.availableEventMaps.get(eventMapKey).events.forEach(eventData => {
-        EventHandler.eventMap.set(`${ eventData.useObj } ${ eventData.withObj }`, eventData.event);
+        EventHandler.eventMap.set(`${ eventData.useObj } ${ eventData.withObj }`, eventData.event); // Where the key format is defined TODO change to a more robust key system
       });
     }
   }
 
+  /**
+   * Attempts to run an event given two objects
+   * @param useObject The object being used
+   * @param withObject The object being used on
+   * @returns The outcome of the event
+   */
   static runEvent(useObject: GameObject, withObject: GameObject): string {
     let key = `${ useObject.id } ${ withObject.id }`;
     if (EventHandler.eventMap.has(key)) {
--- src/handler/inventoryHandler.ts
+++ src/handler/inventoryHandler.ts
@@ -1,13 +1,19 @@
 import { GameObject } from "../gameobjects/gameObject";
 
+/**
+ * Handles all interaction with the player's inventory, or how they store objects.
+ * Inventory handler acts as a singleton
+ */
 export class InventoryHandler {
-  private static instance: InventoryHandler;
+  private static instance: InventoryHandler; // The singleton instance of the handler
 
-  objectsInInventory: Array<GameObject>;
-  size: number;
+  objectsInInventory: Array<GameObject>; // Describes what objects are kept in the inventory
+  size: number; // Describes how many objects are in the inventory
 
-  // This class's instance must be procured before running commands because
-  //  we don't know if / when this class will be instantiated
+  /**
+   * Returns the instance of InventoryHandler
+   * This class's instance must be procured before running commands because we don't know if / when the class instance will be instantiated
+   */
   static getInstance(): InventoryHandler {
     if (InventoryHandler.instance === undefined) {
       InventoryHandler.instance = new InventoryHandler();
@@ -15,6 +21,12 @@ export class InventoryHandler {
     return InventoryHandler.instance;
   }
 
+  /**
+   * Get all objects that respond to the name given
+   * Will be an array of objects because object name is not unique
+   * @param objName A string representing the object's name
+   * @returns An array containing the game objects with the name (or alternate name) given
+   */
   getObjects(objName: string): Array<GameObject> {
     let objs = new Array<GameObject>();
     this.objectsInInventory.forEach(obj => {
@@ -25,11 +37,20 @@ export class InventoryHandler {
     return objs;
   }
 
+  /**
+   * Adds an object to the inventory
+   * @param obj The game object to add to the inventory
+   */
   addObject(obj: GameObject) {
     this.objectsInInventory.push(obj);
     this.size += 1;
   }
 
+  /**
+   * Removes an object from the inventory
+   * @param obj The object to remove from the inventory
+   * @returns True if the object was successfully removed, false otherwise
+   */
   removeObject(obj: GameObject): boolean {
     const index = this.objectsInInventory.indexOf(obj);
     if (index != -1) {
@@ -40,6 +61,9 @@ export class InventoryHandler {
     return false;
   }
 
+  /**
+   * Creates a new instance of the InventoryHandler by instantiating the 
+   */
   private constructor() {
     this.objectsInInventory = new Array<GameObject>();
     this.size = 0;
