455eebffc5a5228491a77b662928cdc8d81c1e4d
justinro-underscore <jcroderman@gmail.com>
2020-04-19 17:38:59 -0500

    Changes main menu

--- src/scenes/mainMenuScene.ts
+++ src/scenes/mainMenuScene.ts
@@ -1,4 +1,5 @@
 import "phaser";
+import { TerminalInputHandler } from "../handler/terminalInputHandler";
 import { MapHandler } from "../handler/mapHandler";
 import { EventHandler } from "../handler/eventHandler";
 
@@ -7,8 +8,7 @@ import { EventHandler } from "../handler/eventHandler";
  */
 export class MainMenuScene extends Phaser.Scene {
   title: Phaser.GameObjects.Text; // Text which holds the title of the game
-  authors: Phaser.GameObjects.Text; // Text which holds the authors of the game
-  start: Phaser.GameObjects.Text; // Text that tells the user how to start the game
+  terminalText: Phaser.GameObjects.Text; // Text that shows various information
 
   constructor() {
     super({
@@ -17,49 +17,30 @@ export class MainMenuScene extends Phaser.Scene {
   }
 
   /**
-   * Loads the images
-   */
-  preload() {
-    this.load.image("blackhole", "assets/img/black-hole-medium.jpg");
-    this.load.image("ship", "assets/img/train-space-ship.png");
-  }
-
-  /**
    * Set up all game assets shown to user and adds functionality
    */
   create() {
-    /*************************
-     * Set up the background *
-     *************************/
-    this.add.image(200, 350, "blackhole");
-    this.add.image(620, 520, "ship").setScale(-0.2, 0.2).setAngle(10);
-
     /************
      * Add text *
      ************/
-    this.title = this.add.text(110, 0, "Voyage of the Infinity",
-      { font: "62px Trade Winds", fill: "#fbfbac" });
-    this.authors = this.add.text(200, 60, "Created by Justin and Sean",
-      { font: "42px Trade Winds", fill: "#fbfbac" });
-    this.start = this.add.text(10, 450, "Press space to start...",
-      { font: "32px Trade Winds", fill: "#fbfbac" });
+    this.title = this.add.text(15, 15, "Voyage of the Infinity",
+      { font: "42px Monospace", fill: "#fbfbac" });
+    this.terminalText = this.add.text(15, 15 + this.title.height + 5, "Created by Justin Roderman and Sean Poole\nType \"start\" to begin the game",
+      { font: "24px Monospace", fill: "#fbfbac" });
 
     /*********************
-     * Add functionality *
+     * Add terminal input *
      *********************/
-    this.input.keyboard.on("keydown", function (event: KeyboardEvent) {
-      if (event.keyCode === 32) { // Space starts the game
-        // Set up the game
-        MapHandler.instantiateInstance("testing");
-        EventHandler.instantiateEventMap("testing");
-        // Start the game
-        this.scene.start("TerminalScene");
-      }
-      else if (event.key === "D") { // D goes to debug scene
-        MapHandler.instantiateInstance("testing");
-        EventHandler.instantiateEventMap("testing");
-        this.scene.start("DebugMapScene");
-      }
-    }, this);
+    TerminalInputHandler.instantiateTerminalInput(this, MainMenuScene.onEnter, { fontSize: 24 });
+  }
+
+  private static onEnter(inputStr: string, scene: Phaser.Scene) {
+    if (inputStr === "start") {
+      // Set up the game
+      MapHandler.instantiateInstance("testing");
+      EventHandler.instantiateEventMap("testing");
+      // Start the game
+      scene.scene.start("TerminalScene");
+    }
   }
 }
